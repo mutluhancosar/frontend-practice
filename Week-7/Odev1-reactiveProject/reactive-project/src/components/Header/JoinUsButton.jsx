667f6e63e0eea6a651f66e7d6bb6a633b51829fa
mutluhancosar <mutluhancosar.1903@gmail.com>
2024-09-28 21:22:58 +0300

    last commit

---
+++ Week-7/Odev1-reactiveProject/reactive-project/src/components/Header/JoinUsButton.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+
+const JoinUsButton = () => {
+  return (
+    <button className="join-us-button">Join Us</button>
+  );
+};
+
+export default JoinUsButton;
---
+++ Week-7/Odev1-reactiveProject/reactive-project/src/components/Header/Navbar.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>Home</li>
+        <li>Classes</li>
+        <li>Trainers</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
